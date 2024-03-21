e81abde035e42720bae784a7036ea6921ed8281b
Eugene Stadnik <eugene.s@fintopio.com>
2024-03-21 15:44:34 +0200

    change home page

--- apps/main/pages/index.tsx
+++ apps/main/pages/index.tsx
@@ -1,11 +1,6 @@
 import type { NextPage } from 'next'
-import useTranslation from 'next-translate/useTranslation'
 import { BaseLayout } from '@/layout/BaseLayout'
 
-const Home: NextPage = () => {
-  const { t } = useTranslation('auth')
-
-  return <BaseLayout> {t('view-more')}</BaseLayout>
-}
+const Home: NextPage = () => <BaseLayout />
 
 export default Home
