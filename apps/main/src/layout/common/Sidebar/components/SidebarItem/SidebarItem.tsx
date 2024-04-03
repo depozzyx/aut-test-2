2d49edcc9a27b83b166ba50d2c85e4122d6246c8
Oleksandr Hrytsenko <peiko.hrytsenko.o@gmail.com>
2024-04-04 02:10:47 +0300

    fix sidebar styles

--- apps/main/src/layout/common/Sidebar/components/SidebarItem/SidebarItem.tsx
+++ apps/main/src/layout/common/Sidebar/components/SidebarItem/SidebarItem.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { SidebarItemIcon } from '@peiko/components/icons/SidebarItemIcon'
 import { NextLink } from '@peiko/components/links/NextLink'
 import { Text } from '@peiko/components/Text'
@@ -10,15 +11,18 @@ interface ISidebarItemProps {
 }
 
 export const SidebarItem = ({ title, link, onClick }: ISidebarItemProps): JSX.Element => {
+  const { pathname } = useRouter()
+
   const content = (
     <ItemWrapper
       height={74}
-      padding="8px 10px"
+      padding="8px 20px"
       direction="column"
       align="center"
       justify="center"
       cursor="pointer"
       onClick={onClick}
+      isSelected={pathname === link}
     >
       <SidebarItemIcon />
       <Text variant="f10" color="base">
--- apps/main/src/layout/common/Sidebar/styles/Sidebar.styled.ts
+++ apps/main/src/layout/common/Sidebar/styles/Sidebar.styled.ts
@@ -4,10 +4,10 @@ import { Flex } from '@/components/Flex'
 export const Container = styled(Flex)(
   ({ theme }) => css`
     min-height: 'calc(100vh - var(--header-height))';
-    flex: 0 0 100px;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
     background-color: ${theme.palette.main4};
+    /* flex: 0 0 100px; */
   `,
 )
