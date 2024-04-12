5e9dbe436eee58cce21ca00a18fc7efbb45ec92b
Oleksandr Hrytsenko <peiko.hrytsenko.o@gmail.com>
2024-04-12 08:36:32 +0300

    minor changes

--- apps/main/src/features/campaigns/containers/NewCampaignReviewModal/components/ReviewFields/ReviewFields.tsx
+++ apps/main/src/features/campaigns/containers/NewCampaignReviewModal/components/ReviewFields/ReviewFields.tsx
@@ -1,32 +1,45 @@
 import { Flex } from '@/components/Flex'
-// import { useRedux } from '@/hooks/use-redux'
+import { useRedux } from '@/hooks/use-redux'
 import useTranslation from 'next-translate/useTranslation'
 import { OutlinedButton } from '@peiko/components/buttons/OutlinedButton'
 import { FilledButton } from '@peiko/components/buttons/FilledButton'
 import useModals from '@/features/common/modals/hooks/use-modals'
-// import { selectCreateCampaignFormData } from '@/features/campaigns/store/create-campaign'
-// import { Field } from './ReviewFields.styled'
+import { MODAL_NAMES } from '@/features/common/modals/constants'
+import { selectCreateCampaignFormData } from '@/features/campaigns/store/create-campaign'
+import { Field } from './ReviewFields.styled'
 
 export const ReviewFields = (): JSX.Element => {
   const { t } = useTranslation('campaigns')
   const { resetModals, setModal } = useModals()
-  //   const { select } = useRedux()
-  //   const formData = select(selectCreateCampaignFormData)
+  const { select } = useRedux()
+  const formData = select(selectCreateCampaignFormData)
 
   const handleBack = () => {
-    setModal({ modalName: 'CREATE_CAMPAIGN', isOpen: true })
+    setModal({ modalName: MODAL_NAMES.CREATE_CAMPAIGN, isOpen: true })
   }
 
   return (
     <Flex direction="column" align="center" gap={48} margin="40px 0 0 0">
-      <Flex gap={24} direction="column">
-        {/* <Field label={} value={} />
-        <Field label={} value={} />
-        <Field label={} value={} />
-        <Field label={} value={} />
-        <Field label={} value={} />
-        <Field label={} value={} />
-        <Field label={} value={} /> */}
+      <Flex gap={24} direction="column" width="326px">
+        <Field label={t('create-campaign.campaign-name')} value={formData.name} />
+        <Field
+          label={t('create-campaign.agent-assignment')}
+          value={formData.assignedAgentIds}
+        />
+        <Field
+          label={t('create-campaign.lead-selection')}
+          value={formData.leadSelection}
+        />
+        <Field label={t('create-campaign.creation-date')} value={formData.creationDate} />
+        <Field
+          label={t('create-campaign.scenario-setup')}
+          value={formData.scenarioSetup}
+        />
+        <Field
+          label={t('create-campaign.call-frequency')}
+          value={formData.callFrequency}
+        />
+        <Field label={t('create-campaign.time-for-calls')} value={formData.callTime} />
       </Flex>
       <Flex align="center" justify="center" gap={24}>
         <OutlinedButton onClick={handleBack} width="236px">
--- apps/main/src/features/campaigns/store/create-campaign.ts
+++ apps/main/src/features/campaigns/store/create-campaign.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit'
-import { TSelector } from '@/store'
+import { TSelector, TAsyncAction } from '@/store'
 import { IinitialValues } from '@/features/campaigns/containers/CreateCampaignModal/components/CreateCampaignForm/CreateCampaignForm'
+import { modalsActions } from '@/features/common/modals/store'
 
 export type TInit = {
   formData: Record<string, unknown> | IinitialValues
@@ -33,3 +34,10 @@ export const selectCreateCampaignFormData: TSelector<TInit['formData']> = create
 )
 
 export default createCampaign.reducer
+
+export const createCampaignAsync =
+  (formData: IinitialValues): TAsyncAction =>
+  async (dispatch) => {
+    dispatch(setFormData(formData))
+    dispatch(modalsActions.setModal({ modalName: 'REVIEW_CAMPAIGN', isOpen: true }))
+  }
