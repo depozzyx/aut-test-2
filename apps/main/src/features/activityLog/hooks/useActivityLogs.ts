0ca0cace7ddfc6e4d467d91622dc3e6457991569
Viacheslav Yagorlytskyi <111735205+ViacheslavYahorlytskyi@users.noreply.github.com>
2024-05-02 12:09:40 +0300

    I added hot fix to activity logs

--- apps/main/src/features/activityLog/hooks/useActivityLogs.ts
+++ apps/main/src/features/activityLog/hooks/useActivityLogs.ts
@@ -41,7 +41,7 @@ export const useActivityLogs = (): {
     getActivityLogsAsync({
       page: pagination.page,
       limit: pagination.limit,
-      orderBy: 'ASC',
+      orderBy: 'DESC',
     })
   }, [])
 
