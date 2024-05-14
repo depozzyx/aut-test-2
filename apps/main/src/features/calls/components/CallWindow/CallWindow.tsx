a6d2a6409ae3a0a50975192ca0efbf88afa0a966
Viacheslav Yagorlytskyi <111735205+ViacheslavYahorlytskyi@users.noreply.github.com>
2024-05-14 18:29:15 +0300

    I added timer to CallWindow

--- apps/main/src/features/calls/components/CallWindow/CallWindow.tsx
+++ apps/main/src/features/calls/components/CallWindow/CallWindow.tsx
@@ -1,25 +1,23 @@
-/* eslint-disable arrow-body-style */
 /* eslint-disable i18next/no-literal-string */
 import React, { FC } from 'react'
 import { Flex } from '@/components/Flex'
 import { Text } from '@peiko/components/Text'
-// import { useStopwatch } from 'react-timer-hook'
+import { useStopwatch } from 'react-timer-hook'
 import { Wrapper } from './CallWindow.styled'
 
 export const CallWindow: FC = () => {
-  // const { seconds, minutes, hours, start, pause, reset } = useStopwatch({
-  //   autoStart: true,
-  // })
+  const { seconds, minutes, hours } = useStopwatch({
+    autoStart: true,
+  })
 
-  // const formatData = (value: number) => value.toString().padStart(2, '0')
+  const formatData = (value: number) => value.toString().padStart(2, '0')
 
   return (
     <Flex gap="8px" align="center" direction="column">
       <Wrapper justify="center" align="center">
         <Text variant="f2">
-          {/* <span>{formatData(hours)}</span>:<span>{formatData(minutes)}</span>:
-          <span>{formatData(seconds)}</span> */}
-          00:00:00
+          <span>{formatData(hours)}</span>:<span>{formatData(minutes)}</span>:
+          <span>{formatData(seconds)}</span>
         </Text>
       </Wrapper>
       <Text>Ashley Cooper</Text>
