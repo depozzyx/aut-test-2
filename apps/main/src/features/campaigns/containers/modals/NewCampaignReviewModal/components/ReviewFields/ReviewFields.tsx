bf6e0a70fac7354c1dc7d4d41808d5899811b19a
Oleksandr Hrytsenko <peiko.hrytsenko.o@gmail.com>
2024-05-19 19:12:39 +0300

    update review modal

--- apps/main/src/features/campaigns/containers/modals/NewCampaignReviewModal/components/ReviewFields/ReviewFields.tsx
+++ apps/main/src/features/campaigns/containers/modals/NewCampaignReviewModal/components/ReviewFields/ReviewFields.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Flex } from '@/components/Flex'
 import { useRedux } from '@/hooks/use-redux'
 import useTranslation from 'next-translate/useTranslation'
@@ -20,8 +21,6 @@ type TProps = {
   type: TCampaignTableType
 }
 
-const getLabelsSting = (labels: string[]): string => labels.join(', ')
-
 export const ReviewFields = ({ type }: TProps): JSX.Element | null => {
   const { t } = useTranslation('campaigns')
   const { setModal } = useModals()
@@ -40,6 +39,26 @@ export const ReviewFields = ({ type }: TProps): JSX.Element | null => {
     dispatch(asyncCreateCampaign(type))
   }
 
+  const agents = useMemo(() => {
+    if (!formDataForReview) return
+    if (formDataForReview.assignedAgentIds.length > 1) {
+      return t('review-campaign.agents', {
+        count: formDataForReview.assignedAgentIds.length + 1,
+      })
+    }
+    return `${formDataForReview.assignedAgentIds}`
+  }, [formDataForReview?.assignedAgentIds])
+
+  const leads = useMemo(() => {
+    if (!formDataForReview) return
+    if (formDataForReview.leadListIdsLabel.length > 1) {
+      return t('review-campaign.leads', {
+        count: formDataForReview.leadListIdsLabel.length + 1,
+      })
+    }
+    return `${formDataForReview.leadListIdsLabel}`
+  }, [formDataForReview?.leadListIdsLabel])
+
   if (!formDataForReview) {
     return null
   }
@@ -51,14 +70,8 @@ export const ReviewFields = ({ type }: TProps): JSX.Element | null => {
           label={t('create-campaign.campaign-name')}
           value={formDataForReview.name}
         />
-        <Field
-          label={t('create-campaign.agent-assignment')}
-          value={formDataForReview.assignedAgentIds}
-        />
-        <Field
-          label={t('create-campaign.lead-selection')}
-          value={getLabelsSting(formDataForReview?.leadListIdsLabel)}
-        />
+        <Field label={t('create-campaign.agent-assignment')} value={agents} />
+        <Field label={t('create-campaign.lead-selection')} value={leads} />
         <Field
           label={t('create-campaign.call-frequency')}
           value={getCallFrequencyLabel(formDataForReview.intensity)}
@@ -72,10 +85,10 @@ export const ReviewFields = ({ type }: TProps): JSX.Element | null => {
       </Flex>
       <Flex align="center" justify="center" gap={24}>
         <OutlinedButton onClick={handleCreateCampaign} width="236px">
-          {t('review-campaign.submit')}
+          {t('review-campaign.submit-btn')}
         </OutlinedButton>
         <FilledButton onClick={handleBack} width="236px">
-          {t('review-campaign.back')}
+          {t('review-campaign.back-btn')}
         </FilledButton>
       </Flex>
     </Flex>
