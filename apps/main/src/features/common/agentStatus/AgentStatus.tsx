ce115bf6d1687472b9a9c29cf7ac23fa12c4a688
Viacheslav Yagorlytskyi <47087934+YagorlitskiyVyacheslav@users.noreply.github.com>
2024-07-18 20:17:16 +0300

    I added pbx status for agent

--- apps/main/src/features/common/agentStatus/AgentStatus.tsx
+++ apps/main/src/features/common/agentStatus/AgentStatus.tsx
@@ -4,7 +4,9 @@ import useTranslation from 'next-translate/useTranslation'
 import React, { FC } from 'react'
 import { TAgentWorkStatus } from '@/features/agents/types'
 import styled from 'styled-components'
+import { Box } from '@peiko/components/Box'
 import { agentActions, agentStatusSelector } from './store'
+import { PBXStatus } from './containers/PBXStatus'
 
 const StyledSelect = styled(Select)`
   .custom-rs__control {
@@ -40,15 +42,18 @@ export const AgentStatus: FC = () => {
   ]
 
   return (
-    <StyledSelect
-      width="105px"
-      name="workStatus"
-      options={options}
-      value={status === 'on-call' ? 'start' : status ?? undefined}
-      onChange={(e) => {
-        if (e?.value && typeof e.value === 'string')
-          dispatch(agentActions.setStatusAsync(e?.value as TAgentWorkStatus))
-      }}
-    />
+    <Box styles={{ display: 'felx', alignItems: 'center', gap: '0' }}>
+      <PBXStatus />
+      <StyledSelect
+        width="105px"
+        name="workStatus"
+        options={options}
+        value={status === 'on-call' ? 'start' : status ?? undefined}
+        onChange={(e) => {
+          if (e?.value && typeof e.value === 'string')
+            dispatch(agentActions.setStatusAsync(e?.value as TAgentWorkStatus))
+        }}
+      />
+    </Box>
   )
 }
