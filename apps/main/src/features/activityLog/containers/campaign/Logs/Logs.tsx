c0dbe0b77af62b6b42f11ecbb9ceea0443b30678
grytsalex <alkatraz0704@gmail.com>
2024-07-24 10:47:36 +0300

    remove unsuported export btn

--- apps/main/src/features/activityLog/containers/campaign/Logs/Logs.tsx
+++ apps/main/src/features/activityLog/containers/campaign/Logs/Logs.tsx
@@ -1,9 +1,7 @@
-import useTranslation from 'next-translate/useTranslation'
 import { shallowEqual } from 'react-redux'
 import { Box } from '@peiko/components/Box'
 import { Pagination } from '@peiko/components/Pagination'
 import { useRedux } from '@/hooks/use-redux'
-import { FilledButton } from '@peiko/components/buttons/FilledButton'
 import { Flex } from '@/components/Flex'
 import { Loader } from '@peiko/components/loaders/Loader/Loader'
 import { createStructuredSelector } from 'reselect'
@@ -18,7 +16,6 @@ import { LogsRecords } from './LogsRecords'
 import { useFetchCampaignLogs } from '../../../hooks/campaign/use-fetch-logs'
 
 export const Logs = (): JSX.Element => {
-  const { t } = useTranslation('activity-log')
   const { dispatch, select } = useRedux()
   useFetchCampaignLogs()
 
@@ -39,13 +36,6 @@ export const Logs = (): JSX.Element => {
   return (
     <Box styles={{ width: '100%', height: '100%' }}>
       <Flex justify="space-between" align="center">
-        <FilledButton
-          // onClick={() => console.log('test')}
-          width="184px"
-          styles={{ height: '32px' }}
-        >
-          {t('export')}
-        </FilledButton>
         <LogsPanel />
       </Flex>
       <LogContentWrapper isLoading={isCLLoading}>
