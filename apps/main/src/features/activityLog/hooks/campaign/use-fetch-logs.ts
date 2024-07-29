f562394703255dbffe8e1c1d4913ca2a7987b6aa
grytsalex <alkatraz0704@gmail.com>
2024-07-29 21:15:20 +0300

    added reset filters btn to campaign-log

--- apps/main/src/features/activityLog/hooks/campaign/use-fetch-logs.ts
+++ apps/main/src/features/activityLog/hooks/campaign/use-fetch-logs.ts
@@ -6,6 +6,8 @@ import { campaignLogsApi } from '@/api-rest/campaign-log'
 import useSWR from 'swr'
 import { handleRestError } from '@/features/common/error'
 import { groupLogsByDate } from '@/features/activityLog/utils/groupLogsByDate'
+import { SORT_BY } from '@/features/campaigns/constants'
+import { ORDER_BY } from '@/constants/orderBy'
 import {
   selectLogPagination,
   selectParams,
@@ -37,6 +39,8 @@ export function useFetchCampaignLogs(): TReturn {
       fetcher({
         page: pagination.page,
         limit: pagination.limit,
+        sortBy: SORT_BY.CREATED_AT,
+        orderBy: ORDER_BY.DESC,
         ...params,
       }),
     {
