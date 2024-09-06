c44f44fc664178fa7681877f120c6a178f5c3045
grytsalex <alkatraz0704@gmail.com>
2024-09-06 10:28:09 +0300

    update manifest to avoid error in console

--- apps/main/src/html/FavIcon/index.tsx
+++ apps/main/src/html/FavIcon/index.tsx
@@ -1,6 +1,6 @@
 export const FavIcon = (): React.ReactElement => (
   <>
-    <link rel="manifest" href="/favicon/site.webmanifest.json" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
     <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="/favicon/favicon-32x32.png" sizes="16x16 32x32" />
   </>
