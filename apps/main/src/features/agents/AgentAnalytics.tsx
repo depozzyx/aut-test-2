946a8981716959ed78347c9b933d75dd76f3741f
grytsalex <alkatraz0704@gmail.com>
2024-10-10 13:42:08 +0300

    change set date for analytics charts

--- apps/main/src/features/agents/AgentAnalytics.tsx
+++ apps/main/src/features/agents/AgentAnalytics.tsx
@@ -1,15 +1,14 @@
+import { useCallback } from 'react'
 import dynamic from 'next/dynamic'
+import { dateToString } from '@/utils/date-to-string'
+import { useRedux } from '@/hooks/use-redux'
 
 import { Flex } from '@/components/Flex'
+import { RangeDayPicker } from '@/inputs/RangeDayPicker'
+import { Loader } from '@peiko/components/loaders/Loader'
 import { AgentNameFilter } from './containers/filters/AgentNameFilter'
 import { useAgentAnalytics } from './hooks/use-agentAnalytics'
-
-const RangeDayPicker = dynamic(
-  () => import('@/inputs/RangeDayPicker').then((mod) => mod.RangeDayPicker),
-  {
-    ssr: false,
-  },
-)
+import { setDateFilter } from './store/agent-analytics'
 
 const CallMinutes = dynamic(
   () => import('./containers/charts/CallMinutes').then((mod) => mod.CallMinutes),
@@ -22,6 +21,7 @@ const CallSuccess = dynamic(
   () => import('./containers/charts/CallSuccess').then((mod) => mod.CallSuccess),
   {
     ssr: false,
+    loading: () => <Loader />,
   },
 )
 
@@ -32,6 +32,7 @@ const AvailabilityOnline = dynamic(
     ),
   {
     ssr: false,
+    loading: () => <Loader />,
   },
 )
 
@@ -42,17 +43,31 @@ const AverageCallDuration = dynamic(
     ),
   {
     ssr: false,
+    loading: () => <Loader />,
   },
 )
 
 export const AgentAnalytics = (): JSX.Element => {
+  const { dispatch } = useRedux()
+
   useAgentAnalytics()
+
+  const handleChangeDate = useCallback((date: { from?: Date; to?: Date }) => {
+    const copyFilters = {
+      ...(date?.from && { fromDate: dateToString(date.from) ?? undefined }),
+      ...(date?.to && { toDate: dateToString(date.to) ?? undefined }),
+    }
+
+    if (Object.keys(copyFilters).length > 0) {
+      dispatch(setDateFilter(copyFilters))
+    }
+  }, [])
 
   return (
     <Flex padding="12px 0 0 0" width="100%">
       <Flex direction="column" gap={12} margin="0 auto" maxWidth={844} width="100%">
         <Flex className="configure-panel" gap={16}>
-          <RangeDayPicker />
+          <RangeDayPicker onChange={handleChangeDate} />
           <AgentNameFilter />
         </Flex>
         <Flex className="charts-container" direction="column" gap={20} width="100%">
