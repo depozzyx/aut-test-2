27816811e61e97041ea375b63ba481b853363daa
Oleksandr Borysenko <oleksandr.borysenko@telqon.io>
2024-10-25 11:57:30 +0300

    revert

--- apps/main/src/layout/common/Sidebar/hooks/use-menu-links.tsx
+++ apps/main/src/layout/common/Sidebar/hooks/use-menu-links.tsx
@@ -31,7 +31,6 @@ const filterMenuItemsByRole = (menuItems: TMenuItem[], userRole: string) =>
 
 export const useMenuLinks = (): TMenuItem[] => {
   const { t } = useTranslation('routing')
-
   const { user } = useAuth()
 
   const links: TMenuItem[] = [
