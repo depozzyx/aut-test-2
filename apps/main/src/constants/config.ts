f240be7481052351252c58c26d5aff9a6913bddb
Oleksandr Borysenko <oleksandr.borysenko@teliqon.io>
2025-01-16 19:23:10 +0000

    PBXC-219 - added decrypt encrypted pbx password for sip connection; rename export to...

--- apps/main/src/constants/config.ts
+++ apps/main/src/constants/config.ts
@@ -4,6 +4,7 @@ export const AXIOS_TIMEOUT = 1000 * 30
 // api
 export const API_REST_URL = `${process.env.NEXT_PUBLIC_API_REST_URL}/`
 export const API_SOCKET_URL = `${process.env.NEXT_PUBLIC_API_SOCKET_URL}/`
+export const API_SECRET_KEY = process.env.NEXT_PUBLIC_API_SECRET_KEY || ''
 
 // Math
 export const MAX_PRECISION = 80
