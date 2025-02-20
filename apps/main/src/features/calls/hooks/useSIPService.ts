fb1b2d9b02319690be604bf8aa0ecd365ffa3845
Oleksandr Borysenko <oleksandr.borysenko@telqon.io>
2025-02-20 19:41:00 +0200

    remove hardcoded pbx domain for prod debug

--- apps/main/src/features/calls/hooks/useSIPService.ts
+++ apps/main/src/features/calls/hooks/useSIPService.ts
@@ -54,7 +54,7 @@ export const useSIPService = (): {
     },
   }
 
-  const jsSIPSocket = new JsSIP.WebSocketInterface(`wss://pbx-core.voipenv.uk:7777/ws`)
+  const jsSIPSocket = new JsSIP.WebSocketInterface(`wss://${pbxAuth?.domain}:7777/ws`)
   console.warn(`wss://${pbxAuth?.domain}:7777/ws`)
   const [ua, setUA] = useState<UA | null>(null)
 
